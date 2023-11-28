3400913e590021d6a1dc4d83f23323eeb22d32b6
maarten <van_den_bergmaarten@hotmail.com>
2023-11-28 12:40:03 +0100

    added some styling

--- src/pages/searchResults.tsx
+++ src/pages/searchResults.tsx
@@ -27,16 +27,17 @@ const Checkbox = () => {
          <div className="flex-shrink-0 w-72">
   {isChecked ? ( <h1 className="text-5xl font-bold mb-0 text-white">Show Perks</h1> ) : ( <h1 className="text-5xl font-bold mb-0 text-white">Show NFTs</h1> )} 
   </div>
-    <label className="relative inline-flex items-center cursor-pointer ml-4"> 
+    <label className="relative inline-flex items-center cursor-pointer">
       <input
-        type="checkbox"
-        value=""
         className="sr-only peer"
+        value=""
+        type="checkbox"
         checked={isChecked}
         onChange={handleCheckboxChange}
       />
-      <div className={`group peer ring-0 ${isChecked ? 'bg-gradient-to-r from-emerald-500 to-emerald-900 peer-checked' : 'bg-gradient-to-r from-rose-400 to-red-900'} rounded-full outline-none duration-1000 after:duration-300 w-24 h-12 shadow-md peer-focus:outline-none after:content-[''] after:rounded-full after:absolute after:[background:#0D2B39] peer-checked:after:rotate-180 after:[background:conic-gradient(from_135deg,_#b2a9a9,_#b2a8a8,_#ffffff,_#d7dbd9_,_#ffffff,_#b2a8a8)] after:outline-none after:h-10 after:w-10 after:top-1 after:left-1 peer-checked:after:translate-x-12 peer-hover:after:scale-95 peer-checked:bg-gradient-to-r peer-checked:from-emerald-500 peer-checked:to-emerald-900`}
-      ></div>
+      <div
+className={`group peer ring-aqua-500 ring-4 bg-gradient-to-bl from-neutral-800 via-neutral-700 to-neutral-600 rounded-full outline-none duration-1000 after:duration-300 w-24 h-12 shadow-md peer-focus:outline-none after:content-[''] after:rounded-full after:absolute after:[background:#0D2B39] peer-checked:after:rotate-180 after:[background:conic-gradient(from_135deg,_#b2a9a9,_#b2a8a8,_#ffffff,_#d7dbd9_,_#ffffff,_#b2a8a8)] after:outline-none after:h-10 after:w-10 after:top-1 after:left-1 peer-checked:after:translate-x-12 peer-hover:after:scale-125`}
+></div>
     </label>
   </div>
 </div>
@@ -45,7 +46,7 @@ const Checkbox = () => {
           {nftData.map((nft) => (
             <div key={nft.id} className="bg-gray-800 p-6 rounded-md"> {/* This is the card*/}
               {isChecked ? (
-                <p className="text-lg h-56 font-bold text-white mb-4">{nft.description}</p>
+                <p className="text-2xl h-56 font-bold text-white mb-4">{nft.description}</p>
               ) : (
                 <img
                   src={nft.imageUrl}
