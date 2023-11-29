70b8beccf1cb5a0b3025e0973e2ddf7c0c1e4d9e
maarten <van_den_bergmaarten@hotmail.com>
2023-11-29 17:48:50 +0100

    added Mint

--- src/pages/mint.tsx
+++ src/pages/mint.tsx
@@ -4,11 +4,20 @@ import Navbar from '../components/Navbar';
 
 const Mint = () => {
     
-
+const [nftName, setNftName] = useState("");
+const [nftSymbol, setNftSymbol] = useState("");
 
 return (
     <>
       <Navbar />
+      <form>
+      <div className="p-8 max-w-screen-xl mx-auto">
+        <input id="nftName" name="nftName" type="text" placeholder="Enter NFT Name" onChange={(e) => setNftName(e.target.value)} value={nftName}/>
+      </div>
+      <div className="p-8 max-w-screen-xl mx-auto">
+        <input id="nftSymbol" name="nftSymbol" type="text" placeholder="Enter NFT Symbol" onChange={(e) => setNftSymbol(e.target.value)} value={nftSymbol}/>
+      </div>
+      </form>
       </>
 );
 
