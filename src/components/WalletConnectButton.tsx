4319ffd60bb71a40ac3a7f56831b8672d211c3cf
maarten <van_den_bergmaarten@hotmail.com>
2023-12-10 14:51:28 +0100

    fixed WalletConnect

--- src/components/WalletConnectButton.tsx
+++ src/components/WalletConnectButton.tsx
@@ -1,6 +1,10 @@
 // components/WalletConnectButton.tsx
-import { ethers } from "ethers";
+import { ethers, Signer } from "ethers";
 import { useEffect, useState } from "react";
+
+interface WalletConnectButtonProps {
+  setSigner: (signer: ethers.Signer) => void;
+}
 
 const WalletConnectButton: React.FC<WalletConnectButtonProps> = ({ setSigner }) => {  const [isConnected, setIsConnected] = useState(false);
   const [hasUpBrowserExtension, setHasUpBrowserExtension] = useState(false);
@@ -18,7 +22,10 @@ const WalletConnectButton: React.FC<WalletConnectButtonProps> = ({ setSigner }) 
         await lukso.request({ method: "eth_requestAccounts" });
         setIsConnected(true);//@ts-ignore
         const provider = new ethers.BrowserProvider(window.lukso);//@ts-ignore
-        setSigner(provider.getSigner());
+        const signer: ethers.Signer = await provider.getSigner();
+                // pass the signer to the setSigner function
+        setSigner(signer);
+        console.log("signer can execute if we log signer here: ", signer); // use it here
       } catch (e) {
         console.log(e);
       }
@@ -27,10 +34,10 @@ const WalletConnectButton: React.FC<WalletConnectButtonProps> = ({ setSigner }) 
     }
   }
 
-  async function execute() {//@ts-ignore
+  async function execute(signer?: Signer) {//@ts-ignore
     if (typeof window.lukso !== "undefined") {
       try {//@ts-ignore
-        console.log("signer can execute if we log signer here: ", setSigner);
+        console.log("signer can execute if we log signer here: ", signer);
       } catch (error) {
         console.log(error);
       }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 // src/pages/index.tsx
 import Link from 'next/link';
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import Navbar from '../components/Navbar';
 import FeaturingCard from '@/components/FeaturingCard';
 import { useState } from "react";
 import { ethers } from "ethers";
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [signer, setSigner] = useState<ethers.Signer | null>(null);
@@ -20,14 +17,12 @@ export default function Home() {
       <h1 className="text-5xl font-bold">
           Discover NFTs <br /> with redeemable <br /> perks
       </h1>
-      <h2 className="text-2xl font-bold">An NFT Search Page to facilitate dynamic searching of traits, enabling users to effortlessly find redeemable perks they are interested in.</h2>
+      <h2 className="text-2xl font-bold">An NFT Search Page to dynamically search traits, enabling users to effortlessly find redeemable perks they are interested in.</h2>
       <Link href="/search" legacyBehavior>
       <button className="button">
         Search
       </button>
-
-    </Link>
-    
+    </Link>    
       </div>
     </main>
   )
